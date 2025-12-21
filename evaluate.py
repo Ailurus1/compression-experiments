8ab05e108947e1624aae0aea24c741dbc73083c3
Ailurus1 <kozulin02@yandex.ru>
2025-12-22 02:20:07 +0300

    Fix evaluation

--- evaluate.py
+++ evaluate.py
@@ -11,6 +11,15 @@ from lighteval.models.transformers.transformers_model import (
 from lighteval.logging.evaluation_tracker import EvaluationTracker
 from lighteval.pipeline import ParallelismManager, Pipeline, PipelineParameters
 
+def get_model_size_gb(model):
+    param_size = sum(p.nelement() * p.element_size() for p in model.parameters())
+    buffer_size = sum(b.nelement() * b.element_size() for b in model.buffers())
+    size_all_gb = (param_size + buffer_size) / (1024**3)
+    return size_all_gb
+
+def get_model_params(model):
+    return sum(p.numel() for p in model.parameters())
+
 
 def main():
     parser = argparse.ArgumentParser(description="Evaluate models using")
@@ -38,18 +47,19 @@ def main():
 
     args = parser.parse_args()
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
 
     model = AutoModelForCausalLM.from_pretrained(args.path_to_model, device_map="auto")
 
+    print(f"Total parameters: {get_model_params(model)}")
+    print(f"Total model size (Gb): {get_model_size_gb(model)}")
+
     config = TransformersModelConfig(model_name=args.path_to_model, batch_size=1)
     model = TransformersModel.from_model(model, config)
 
     pipeline_params = PipelineParameters(
-        launcher_type=ParallelismManager.NONE, max_samples=args.limit
+        launcher_type=ParallelismManager.ACCELERATE, max_samples=args.limit
     )
 
     pipeline = Pipeline(
@@ -59,22 +69,12 @@ def main():
         tasks=args.tasks,
     )
 
-    results = pipeline.evaluate()
+    pipeline.evaluate()
+    pipeline.show_results()
 
-    for task_name, task_results in results.items():
-        if "results" in task_results:
-            print(f"\n{task_name}:")
-            for metric, value in task_results["results"].items():
-                print(f"  {metric}: {value}")
-        else:
-            print(f"\n{task_name}: No results available")
-
-    results_file = output_dir / "results.json"
-
-    with open(results_file, "w", encoding="utf-8") as file:
-        json.dump(results, file, indent=2)
-
-    print(f"Results are saved in: {results_file}")
+    result_dict = pipeline.get_results()
+    with open(Path(args.output_dit) / 'results.json', 'w', encoding="utf-8") as file:
+        json.dump(result_dict, file)
 
 
 if __name__ == "__main__":
